35643f98c15a52ead6c4838d9b4d17f57c740c23
hieza <80311214+hieza@users.noreply.github.com>
2021-03-09 11:05:07 +0300

    Add files via upload

---
+++ задача 6.cpp
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int ml;
+    float km;
+    cout<<"введите количество миль"<<endl;
+    cin>>ml;
+    km=ml*1.066;
+    cout<<km<<" километров"<<endl;
+    return 0;
+}
